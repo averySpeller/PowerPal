aa37188e28c8cd9a83050c7a79340f7a7b17cd90
AverySpeller <avery.speller@gmail.com>
2018-03-13 15:46:48 -0400

    Initial Commit

---
+++ PowerPal/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  PowerPal
+//
+//  Created by Avery Speller on 2018-03-13.
+//  Copyright © 2018 Avery Speller. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
